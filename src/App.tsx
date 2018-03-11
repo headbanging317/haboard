bcb7d9b747634494486df8d1901928ed4c34c566
headbanging3 <24thelf@naver.com>
2018-03-11 22:19:41 +0900

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+import Addlist from './components/Addlist';
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Addlist />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Addlist.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+interface Props {
+
+}
+
+const Addlist: React.SFC<Props> = () => (
+    <div>
+        <button>Add a List</button>
+    </div>
+);
+
+export default Addlist;
